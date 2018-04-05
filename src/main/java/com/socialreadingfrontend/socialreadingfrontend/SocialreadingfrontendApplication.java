0937d365cff1292a00a4d50686d82350bb2905c3
LAPTOP-574ELQ60\Jony <jmm92.jm@gmail.com>
2018-04-04 21:52:40 -0300

    first draft project

---
+++ src/main/java/com/socialreadingfrontend/socialreadingfrontend/SocialreadingfrontendApplication.java
@@ -0,0 +1,12 @@
+package com.socialreadingfrontend.socialreadingfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialreadingfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialreadingfrontendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/socialreadingfrontend/socialreadingfrontend/SocialreadingfrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.socialreadingfrontend.socialreadingfrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialreadingfrontendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
